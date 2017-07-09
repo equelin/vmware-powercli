824a8240fce07650855cf334d36bda892aa76c30
Erwan Quelin <erwan.quelin@gmail.com>
2017-07-09 22:28:42 +0200

    Added custom shares

--- VDS/Set-VDSInfrastructureTrafficResourceConfig.ps1
+++ VDS/Set-VDSInfrastructureTrafficResourceConfig.ps1
@@ -1,6 +1,6 @@
 <#
     .SYNOPSIS 
-    Configure system traffic ressource.
+    Configure system traffic ressource for distributed virtual switchs.
     .NOTES
     Written by Erwan Quelin under MIT licence.
     .PARAMETER VDS
@@ -8,30 +8,40 @@
     .PARAMETER Type
     Traffic type (management, vmotion, vsan...).
     .PARAMETER Shares
-    Shares (low, normal, high).
+    Shares (low, normal, high or custom).
     .EXAMPLE
     .\Set-VDSInfrastructureTrafficResourceConfig.ps1 -VDS 'VDS01' -Type 'vsan' -Shares 'high'
 
     Set the 'vsan' traffic resource config to 'normal'.
+    .EXAMPLE
+    .\Set-VDSInfrastructureTrafficResourceConfig.ps1 -VDS 'VDS01' -Type 'vsan' -Shares 'custom' -Value 150
+
+    Set the 'vsan' traffic resource config to a 'custom' value of 150.
     .EXAMPLE
     Get-VDSwitch -Name VDS01 | .\Set-VDSInfrastructureTrafficResourceConfig.ps1 -Type vdp -Shares low
 
     Set the 'vdp' traffic resource config to 'low' by providing the VDS through a pipeline. 
 #>
 
-[CmdletBinding(SupportsShouldProcess = $True,ConfirmImpact = 'High')] Param (
+[CmdletBinding(SupportsShouldProcess = $True,ConfirmImpact = 'High')] 
+Param (
     [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
     [Object]$VDS,
+    [Parameter(Mandatory=$true)]
     [ValidateSet('management','faultTolerance','vmotion','virtualMachine','iSCSI','nfs','hbr','vsan','vdp')]
     [String]$Type,
-    [ValidateSet('low','normal','high')]
-    [String]$Share
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('low','normal','high','custom')]
+    [String]$Share,
+    [Parameter(Mandatory=$false)]
+    [uint32]$Value
 )
 
 Switch ($VDS.GetType().Name) {
     'String' {$VDS = Get-VDSwitch -Name $VDS -ErrorAction SilentlyContinue}
-    'VmwareVDSwitchImpl' {$VDS = $VDS}
-    default {Throw 'Please provide an valid VDS name or object'} }
+    'VmwareVDSwitchImpl' {$VDS = Get-VDSwitch -Name $VDS -ErrorAction SilentlyContinue}
+    default {Throw 'Please provide an valid VDS name or object'} 
+}
 
 If (Get-VDSwitch -Name $VDS.Name -ErrorAction SilentlyContinue) {
 
@@ -46,6 +56,11 @@ If (Get-VDSwitch -Name $VDS.Name -ErrorAction SilentlyContinue) {
 
     #Modify the traffic resource config with informations provided by parameters
     ($DVSConfigSpec.InfrastructureTrafficResourceConfig | where-object {$_.key -match $Type}).AllocationInfo.Shares.Level = $Share
+
+    #Set the value of the custom share if specified
+    If ($Share -eq 'custom') {
+        ($DVSConfigSpec.InfrastructureTrafficResourceConfig | where-object {$_.key -match $Type}).AllocationInfo.Shares.Shares = $Value
+    }
 
     #Reconfigure the DVS
     If ($pscmdlet.ShouldProcess($VDS.Name,"Modify traffic $Type with value $Share")) {
